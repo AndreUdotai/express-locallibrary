e11a023857fb7278d87d90cb28673e6c446d170a
Ubong Udotai <31491736+AndreUdotai@users.noreply.github.com>
2022-10-06 23:34:39 +0100

    updated app,js with the catalog routes

--- app.js
+++ app.js
@@ -6,17 +6,19 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
 
 var app = express();
 
 // Set up mongoose connection
-// This code creates the default connection to the database and binds to the error 
+// This code creates the default connection to the database and binds to the error
 // event (so that errors will be printed to the console).
-const mongoose = require("mongoose");
-const mongoDB = "mongodb+srv://andreudotai:mineshadow@cluster0.r368mde.mongodb.net/local_library?retryWrites=true&w=majority";
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+const mongoose = require('mongoose');
+const mongoDB =
+    'mongodb+srv://andreudotai:mineshadow@cluster0.r368mde.mongodb.net/local_library?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error"));
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -30,21 +32,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
