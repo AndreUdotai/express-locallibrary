c55bcf1605dd62e342a205ab16e66ada9c1666f5
Ubong Udotai <31491736+AndreUdotai@users.noreply.github.com>
2022-10-10 12:14:31 +0100

    created author list view page and updated authorControllercodes to fetch list of authors from db

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -21,9 +21,10 @@ BookInstanceSchema.virtual('url').get(function () {
     return `/catalog/bookinstance/${this._id}`;
 });
 
+// Virtual for Due back date
 BookInstanceSchema.virtual("due_back_formatted").get(function () {
     return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
-  });
+});
 
 // Export model
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
